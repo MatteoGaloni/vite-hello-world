f02757656a69d2fece34dfcea05882acdff9ecdc
MatteoGaloni <mtgaloni@gmail.com>
2023-05-18 19:10:19 +0200

    first commit

---
+++ src/components/AppHero.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+name : "AppHero"
+
+}
+</script>
+
+<template>
+    <div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRD60v898I37hvwOhjAnpFYTRggKwhkOVSBLg&usqp=CAU" alt="jumbotron">
+    </div>
+</template>
+
+
+<style>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: "NewComponent",      
+      
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        addbutton() {
+    console.log("CIAO")
+}
+        
+    }
+}
+
+</script>
+
+
+<template>
+    <h1>PROVA GENERALE SUPREMA</h1>
+    <h2>La mia prima app con Vite!</h2>
+    <button @click="addbutton()">CLICCAMI</button>
+</template>
